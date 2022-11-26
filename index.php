076e7ec55b9d8149209b01fb464166365834b8d3
kaleab <808kaleabkzin@gmail.com>
2022-11-26 14:10:06 -0500

    initial

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+   $con = mysqli_connect("localhost", "phpmyadmin", "some_pass", "social");
+
+   if(mysqli_connect_errno()){
+      echo "failed to connect" . mysqli_connect_errno();
+   }
+
+   $query = mysqli_query($con, "INSERT INTO test VALUES(NULL, 'max')");
+?>
+
+<html>
+   <head>
+      <title></title>
+   </head>
+
+   <body>
+        hello world
+   </body>
+</html>
